8b4bfb648c75a311f40a78a53ef2ae246d725a62
Rubén <hey@rubenvara.io>
2023-03-12 14:45:04 +0100

    tidyup

---
+++ src/lib/gorroColors.ts
@@ -0,0 +1,18 @@
+export const gorroColors = [
+	{ value: '#cfc100', label: 'Amarillo' },
+	{ value: '#dedede', label: 'Blanco' },
+	{ value: '#cb620f', label: 'Naranja' },
+	{ value: '#bc4033', label: 'Rojo' },
+	{ value: '#dc4491', label: 'Rosa' },
+	{ value: '#8f2b29', label: 'Burdeos' },
+	{ value: '#87c6e7', label: 'Celeste' },
+	{ value: '#0059c1', label: 'Royal' },
+	{ value: '#072e61', label: 'Marino' },
+	{ value: '#a0a3a2', label: 'Plata' },
+	{ value: '#c6ad69', label: 'Oro' },
+	{ value: '#2b2b2b', label: 'Negro' },
+	{ value: '#d2e73e', label: 'Flúor' },
+	{ value: '#7dc764', label: 'Verde Hierba' },
+	{ value: '#329c72', label: 'Verde Metal' },
+	{ value: '#3ca642', label: 'Verde Benetton' }
+];
